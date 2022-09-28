542ad8a9ef8b9f6cb8a5ccdec661de7a9c6795fa
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-28 15:49:45 +0100

    Initial commit...

---
+++ utils/Projects.js
@@ -0,0 +1,86 @@
+const projects = [
+  {
+    logo: "",
+    name: "Fetiino",
+    description: "Color tool for designers and developers",
+    url: "https://fetiino.netlify.app",
+    emoji: "🎨",
+    tags: [
+      {
+        icon: "",
+        title: "Won Hashnode hackathon",
+      },
+      {
+        icon: "",
+        title: "Turned down $7k acquisition offer",
+      },
+    ],
+  },
+  {
+    logo: "",
+    name: "Quickk",
+    description:
+      "Blogging platform with cryptocurrency donation infrastructure.",
+    url: "https://quickk.blog",
+    emoji: "⚡",
+    tags: [
+      {
+        icon: "",
+        title: "Didn't win hackathon",
+      },
+      {
+        icon: "",
+        title: "50+ users in 24 hours of launch",
+      },
+      {
+        icon: "",
+        title: "$0 MRR",
+      },
+    ],
+  },
+  {
+    logo: "",
+    name: "MessageMe",
+    description: "Anonymous messaging platform",
+    url: "https://messageme.me",
+    emoji:
+      "https://cdn.joypixels.com/products/previews/O6D7BMG8R2DMMNC4LLZH/2332_RCzlB4jrR8ETuzqKU0wVXmu1qBQP5SuN.gif",
+    tags: [
+      {
+        icon: "",
+        title: "Built in 2 weeks",
+      },
+      {
+        icon: "",
+        title: "300+ users",
+      },
+      {
+        icon: "",
+        title: "My friends love it",
+      },
+    ],
+  },
+  {
+    logo: "",
+    name: "CtrlV",
+    description: "Cross-platform file, texts and links sharing tool.",
+    url: "https://ctrlv.app",
+    emoji: "🚀",
+    tags: [
+      {
+        icon: "",
+        title: "10 platforms",
+      },
+      {
+        icon: "",
+        title: "Got a co-founder",
+      },
+      {
+        icon: "",
+        title: "Coming soon",
+      },
+    ],
+  },
+];
+
+export default projects;
